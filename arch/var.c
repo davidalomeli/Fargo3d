da92d81936b675557b47aa392c8c0fcffb9cc414
davidalomeli <dlomeli@miners.utep.edu>
2026-09-08 21:23:58 -0600

    Changed Nr to 2048

--- arch/var.c
+++ arch/var.c
@@ -35,13 +35,13 @@ void InitVariables() {
   init_var("NTOT", (char*)&NTOT, INT, NO, "500");
   init_var("NU", (char*)&NU, REAL, NO, "0.0");
   init_var("NX", (char*)&NX, INT, NO, "1");
-  init_var("NY", (char*)&NY, INT, NO, "1024");
+  init_var("NY", (char*)&NY, INT, NO, "2048");
   init_var("NZ", (char*)&NZ, INT, NO, "1");
   init_var("OHMICDIFFUSIONCOEFF", (char*)&OHMICDIFFUSIONCOEFF, REAL, NO, "0.0");
   init_var("OMEGAFRAME", (char*)&OMEGAFRAME, REAL, NO, "0.0");
   init_var("OORTA", (char*)&OORTA, REAL, NO, "-0.75");
   init_var("ORBITALRADIUS", (char*)&ORBITALRADIUS, REAL, NO, "0.0");
-  init_var("OUTPUTDIR", (char*)&OUTPUTDIR, STRING, NO, "/project/phys-k/fargo_share/fargo_gasdisk_alldamp_viscr_lowdamp_hires");
+  init_var("OUTPUTDIR", (char*)&OUTPUTDIR, STRING, NO, "/project/phys-k/fargo_share/fargo_gasdisk_alldamp_viscr_lowdamp_hihires");
   init_var("PARTICLESIZE1", (char*)&PARTICLESIZE1, REAL, NO, "0.1");
   init_var("PERIODICY", (char*)&PERIODICY, BOOL, NO, "0");
   init_var("PERIODICZ", (char*)&PERIODICZ, BOOL, NO, "0");
