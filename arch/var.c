c94be9c7cb6035d109ab621dec6772bb111956ed
davidalomeli <dlomeli@miners.utep.edu>
2026-09-10 11:06:05 -0600

    Test disabling STOCKHOLM from .opt

--- arch/var.c
+++ arch/var.c
@@ -41,7 +41,7 @@ void InitVariables() {
   init_var("OMEGAFRAME", (char*)&OMEGAFRAME, REAL, NO, "0.0");
   init_var("OORTA", (char*)&OORTA, REAL, NO, "-0.75");
   init_var("ORBITALRADIUS", (char*)&ORBITALRADIUS, REAL, NO, "0.0");
-  init_var("OUTPUTDIR", (char*)&OUTPUTDIR, STRING, NO, "/project/phys-k/fargo_share/fargo_gasdisk_alldamp_viscr_lomiddamp_hihires_superfinedt_openinner_ghost");
+  init_var("OUTPUTDIR", (char*)&OUTPUTDIR, STRING, NO, "/project/phys-k/fargo_share/fargo_gasdisk_nodamp_viscr");
   init_var("PARTICLESIZE1", (char*)&PARTICLESIZE1, REAL, NO, "0.1");
   init_var("PERIODICY", (char*)&PERIODICY, BOOL, NO, "0");
   init_var("PERIODICZ", (char*)&PERIODICZ, BOOL, NO, "0");
